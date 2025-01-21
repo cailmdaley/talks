ae962f951bbf9f3df80b0074f56a91072e03e861
Cail Daley <cailmdaley@gmail.com>
2025-01-21 14:59:31 +0100

    Built site for gh-pages

---
+++ 24_TOSCA_Nice/assets/mathjax.js
@@ -0,0 +1,10 @@
+window.MathJax = {
+    loader: {load: ['[tex]/physics']},
+    tex: {
+        packages: {'[+]': ['physics']},
+        macros: {
+            vbell: '{ \\vb*{\\ell} }',
+            vun:   '{ \\vu{n} }',
+            inv:   '{ ^{-1} }'
+        },
+    }}
